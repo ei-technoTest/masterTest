597fd9d174241dbde67ec02edfb486e21666ce47
Georges <georges.mardini@ei-technologies.com>
2018-10-01 10:16:01 +0200

    test

--- testJenkins.java
+++ testJenkins.java
@@ -2,7 +2,7 @@ public class testJenkins {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("sasa");
+        System.out.println("lolo");
     }
 
 }
